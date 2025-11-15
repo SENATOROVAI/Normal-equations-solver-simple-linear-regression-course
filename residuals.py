490751bb4ffcdf404c926c2ba2fb94c24b0df7a9
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-11-15 03:50:05 +0300

    Update residuals.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- residuals.py
+++ residuals.py
@@ -1,7 +1,3 @@
-import pandas as pd
-
-points = pd.read_csv("https://raw.githubusercontent.com/thomasnield/machine-learning-demo-data/master/regression/single_independent_variable_linear_small.csv", delimiter=",").itertuples()
-
 m = 1.93939394
 b = 4.73333333
 # Вычисляем остатки
